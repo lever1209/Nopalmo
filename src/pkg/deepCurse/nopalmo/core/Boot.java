06b8ce1c5b5c89b3f997248c7470ad4dfe57ec8e
deepCurse <leverplays@gmail.com>
2021-11-29 13:12:36 -0400

    testing .gitignore

--- src/pkg/deepCurse/nopalmo/core/Boot.java
+++ src/pkg/deepCurse/nopalmo/core/Boot.java
@@ -1,11 +1,18 @@
 package pkg.deepCurse.nopalmo.core;
 
+import pkg.deepCurse.simpleLoggingGarbage.core.Log;
+
 public class Boot {
 
 	public static void main(String[] args) {
+		Log.boot("Booting. . .");
+
+		long preBootTime = System.currentTimeMillis();
 		
-		System.out.println("ech");
 		
+		
+		long bootTime = System.currentTimeMillis() - preBootTime;
+
 	}
 
 }
---
+++ src/pkg/deepCurse/simpleLoggingGarbage/core/Log.java
@@ -0,0 +1,25 @@
+package pkg.deepCurse.simpleLoggingGarbage.core;
+
+/**
+ * this class exists for the sole reason of im lazy, as far as i know, this is
+ * really bad practice and i will replace it at some point, or at least upgrade
+ * it, who knows really
+ * 
+ * @author deepCurse
+ */
+public class Log {
+
+	public static int loggerLevel = 0;
+
+	//@formatter:off
+	public static boolean bootEnabled = true;
+	public static void boot(String text) {
+		boot(text,0);
+	}
+	public static void boot(String text, int level) {
+		if (bootEnabled && level <= loggerLevel) {
+			System.out.println(text);
+		}
+	}
+	//@formatter:on
+}
